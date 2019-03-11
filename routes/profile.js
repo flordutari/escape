caa9dcce279cf492a757fcd4beb2d5c128a004ae
Flor <fdutari2@gmail.com>
2019-03-11 14:30:44 +0100

    edit profile done

--- routes/profile.js
+++ routes/profile.js
@@ -6,9 +6,11 @@ const User = require('../models/User');
 
 const { requireUser } = require('../middlewares/index');
 
+const parser = require('../helpers/file-upload');
+
 module.exports = router;
 
-router.get('/:id', requireUser, async (req, res, next) => {
+router.get('/', requireUser, async (req, res, next) => {
   const { _id } = req.session.currentUser;
   try {
     const user = await User.findById(_id);
@@ -17,3 +19,33 @@ router.get('/:id', requireUser, async (req, res, next) => {
     next(error);
   }
 });
+
+router.get('/edit', requireUser, async (req, res, next) => {
+  const { _id } = req.session.currentUser;
+  try {
+    const user = await User.findById(_id);
+    res.render('profile/edit', { _id, user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/edit', requireUser, parser.single('image'), async (req, res, next) => {
+  const { username, description, imageUrl } = req.body;
+  const { _id } = req.session.currentUser;
+  const user = {
+    username,
+    description,
+    imageUrl: req.file.url
+  };
+  console.log(imageUrl);
+  try {
+    await User.findByIdAndUpdate(_id, user);
+    console.log({ _id }, user);
+    res.redirect('/profile');
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
